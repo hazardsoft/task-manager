71d963a74b4f2f0f873fefccdd2b940c576a2e4f
Henadzi Shutko <hazardsoft@gmail.com>
2023-06-11 16:26:12 +0300

    refactoring: users and tasks insert functions are carried into separate modules

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
-import {
-    Collection,
-    Db,
-    InsertManyResult,
-    InsertOneResult,
-    MongoClient,
-    MongoError,
-} from "mongodb";
-import { Task, User } from "./types.js";
+import { Db, MongoClient, MongoError } from "mongodb";
 import { config } from "./config.js";
-import { id } from "./id.js";
+import { addUser } from "./users.js";
+import { addTasks } from "./tasks.js";
 
 const client: MongoClient = new MongoClient(config.connectionUrl);
 try {
@@ -16,11 +9,13 @@ try {
     const db: Db = client.db(config.databaseName);
     console.log("database is connected!");
 
-    const isUserAdded: boolean = await addUser(db);
-    console.log(`user is added: ${isUserAdded}`);
+    // const isUserAdded: boolean = await addUser(db);
+    // console.log(`user is added: ${isUserAdded}`);
 
-    const areTasksAdded: boolean = await addTasks(db);
-    console.log(`tasks are added: ${areTasksAdded}`);
+    // const areTasksAdded: boolean = await addTasks(db);
+    // console.log(`tasks are added: ${areTasksAdded}`);
+
+    
 } catch (e) {
     if (e instanceof MongoError) {
         console.error(`MongoDB error occurred: ${JSON.stringify(e)}`);
@@ -28,51 +23,4 @@ try {
 } finally {
     console.log("connection closed!");
     client.close();
-}
-
-async function addUser(db: Db): Promise<boolean> {
-    const collection: Collection<User> = db.collection(
-        config.usersCollectionName
-    );
-    try {
-        const result: InsertOneResult<User> = await collection.insertOne({
-            _id: id,
-            name: "Henadzi Shutko",
-            email: "hazardsoft@gmail.com",
-            age: 35,
-        });
-        console.log(
-            `user is inserted: ${result.acknowledged}, id ${result.insertedId}`
-        );
-        return true;
-    } catch (e) {
-        console.error(
-            `error occurred while inserting user doc: ${JSON.stringify(e)}`
-        );
-        return false;
-    }
-}
-
-async function addTasks(db: Db): Promise<boolean> {
-    const collection: Collection<Task> = db.collection(
-        config.tasksCollectionName
-    );
-    try {
-        const result: InsertManyResult<Task> = await collection.insertMany([
-            { description: "task1", completed: false },
-            { description: "task2", completed: true },
-            { description: "task3", completed: true },
-        ]);
-        console.log(
-            `tasks are inserted: ${result.acknowledged}, count ${
-                result.insertedCount
-            }, ids ${JSON.stringify(result.insertedIds)}`
-        );
-        return true;
-    } catch (e) {
-        console.error(
-            `error occurred while inserting tasks docs: ${JSON.stringify(e)}`
-        );
-        return false;
-    }
 }
