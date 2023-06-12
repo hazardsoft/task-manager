e7718950c4c1fee5513acce2bc6582bd423df333
Henadzi Shutko <hazardsoft@gmail.com>
2023-06-12 16:25:45 +0300

    mongodb experiments are carried out into mongodb.ts mongoose.ts ground is set

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,8 @@
-import {
-    Db,
-    DeleteResult,
-    MongoClient,
-    MongoError,
-    UpdateResult,
-} from "mongodb";
-import { config } from "./config.js";
-import { addUser } from "./users.js";
-import { addTasks } from "./tasks.js";
-import { Task, User } from "./types.js";
-import {
-    deleteUser,
-    findTask,
-    findTasks,
-    updateTasks,
-    updateUser,
-} from "./crud.js";
+import { run as runMongo } from "./db/mongodb.js";
+import { run as runMongoose } from "./db/mongoose.js";
 
-const client: MongoClient = new MongoClient(config.connectionUrl);
-try {
-    await client.connect();
-    const db: Db = client.db(config.databaseName);
-    console.log("database is connected!");
+// await runMongo();
+// console.log("finished running mongo!");
 
-    // const isUserAdded: boolean = await addUser(db);
-    // console.log(`user is added: ${isUserAdded}`);
-
-    // const areTasksAdded: boolean = await addTasks(db);
-    // console.log(`tasks are added: ${areTasksAdded}`);
-
-    const task: Task | null = await findTask(db);
-    console.log(`findTask result: ${JSON.stringify(task)}`);
-
-    const tasks: Task[] = await findTasks(db);
-    console.log(`findTasks result: ${JSON.stringify(tasks)}`);
-
-    const updatedUser: UpdateResult<User> = await updateUser(db);
-    console.log(
-        `user update result: ack ${updatedUser.acknowledged}, matched ${updatedUser.matchedCount}, modified ${updatedUser.modifiedCount}`
-    );
-
-    const updatedTasks: UpdateResult<Task> = await updateTasks(db);
-    console.log(
-        `tasks update result: ack ${updatedTasks.acknowledged}, matched ${updatedTasks.matchedCount}, modified ${updatedTasks.modifiedCount}`
-    );
-
-    const deletedUser: DeleteResult = await deleteUser(db);
-    console.log(
-        `user delete result: ack ${deletedUser.acknowledged}, deleted ${deletedUser.deletedCount}`
-    );
-} catch (e) {
-    if (e instanceof MongoError) {
-        console.error(`MongoDB error occurred: ${JSON.stringify(e)}`);
-    }
-} finally {
-    console.log("connection closed!");
-    client.close();
-}
+await runMongoose();
+console.log("finished running mongoose!");
