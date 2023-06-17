6811d13cf43f7fb1c497564698540ea40ef1731b
Henadzi Shutko <hazardsoft@gmail.com>
2023-06-17 15:59:07 +0300

    removed cors dep

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 import { router as tasksRouter } from "./routers/tasks.js";
 import { router as usersRouter } from "./routers/users.js";
 import { connect } from "./db.js";
@@ -9,7 +8,6 @@ await connect();
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
-// app.use(cors());
 app.use(tasksRouter);
 app.use(usersRouter);
 app.listen(port);
